02455eb37739a009ae4824a843c5c4f7fa5bcb5a
Chase <chase7106@gmail.com>
2025-04-25 20:02:27 +0800

    8:02下課

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
 import pymysql
@@ -9,10 +9,50 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     # return f"<h1>Hello World!</h1><br>{datetime.now()}"
+    books = [
+        {
+            "name": "Python book",
+            "price": 299,
+            "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+        },
+        {
+            "name": "Java book",
+            "price": 399,
+            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+        },
+        {
+            "name": "C# book",
+            "price": 499,
+            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+        },
+    ]
+
+    books = []
+    if books:
+        for book in books:
+            print(book["name"])
+            print(book["price"])
+            print(book["image_url"])
+    else:
+        print("販售完畢")
+
     username = "chase"
     nowtime = datetime.now().strftime("%Y-%m-%d")
     print(username, nowtime)
-    return render_template("index.html", name=username, now=nowtime)
+    return render_template("index.html", name=username, now=nowtime, books=books)
+
+
+@app.route("/bmi")
+def get_bmi():
+    # args->GET
+    height = eval(request.args.get("height"))
+    weight = eval(request.args.get("weight"))
+
+    print(height, weight)
+
+    bmi = round(weight / (height / 100) ** 2, 2)
+
+    return {"height": height, "weight": weight, "bmi": bmi}
 
 
 @app.route("/pm25-data")
